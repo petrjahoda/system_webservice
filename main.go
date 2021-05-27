23b709366e4407909969e619110e738dee2407a5
petrjahoda <petrjahoda@protonmail.com>
2021-05-27 11:19:30 +0200

    ## [2021.2.2.27] - 2021-05-27
    ### Changed
    
    - removed magic numbers
    
    Took 16 seconds

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 	"os"
 )
 
-const version = "2021.2.2.25"
+const version = "2021.2.2.27"
 const serviceName = "System WebService"
 const serviceDescription = "System web interface"
 const config = "user=postgres password=pj79.. dbname=system host=localhost port=5432 sslmode=disable application_name=system_webservice"
